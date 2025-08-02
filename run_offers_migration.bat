b48e63dff7f2d8f12c3db299e1ffbd0db5c1f0dd
anandakarthick <anandakarthick.s1994@gmail.com>
2025-08-02 22:12:25 +0530

    code push
    code push

--- run_offers_migration.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Running migration to add offer fields to POS sale items...
-cd /d "D:\source_code\ecom"
-php artisan migrate --path=database/migrations/2025_07_26_add_offer_fields_to_pos_sale_items_table.php --force
-echo Migration completed!
-pause
