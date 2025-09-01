0b6d52f9a40a935bd26ac33987e376b5fb6c7feb
anandakarthick <anandakarthick.s1994@gmail.com>
2025-09-01 07:23:16 +0530

    Code push
    Code push

--- clear_cache.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Clearing Laravel cache...
-php artisan cache:clear
-php artisan config:clear
-php artisan view:clear
-php artisan route:clear
-echo Cache cleared successfully!
-pause
